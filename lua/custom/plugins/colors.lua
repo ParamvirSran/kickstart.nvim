3de02fd20811ab9076c8c8324a69c405c088c930
Paramvir Sran <paramvirsran@gmail.com>
2024-07-01 18:51:10 -0400

    added colors

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -1,4 +1,12 @@
 return {
-  { 'rose-pine/neovim', name = 'rose-pine' },
   'folke/tokyonight.nvim',
+  'rebelot/kanagawa.nvim',
+  'EdenEast/nightfox.nvim',
+  { "rose-pine/neovim", name = "rose-pine" },
+  'navarasu/onedark.nvim',
+  'sainnhe/gruvbox-material',
+  'projekt0n/github-nvim-theme',
+  'shaunsingh/nord.nvim',
+  'sainnhe/sonokai',
+  'sainnhe/everforest',
 }
