b2572aff9931188045c27dab4dc1c8c81934b764
Paramvir Sran <paramvirsran@gmail.com>
2024-07-24 16:25:06 -0400

    plugins added to custom folder

---
+++ lua/custom/plugins/colors.lua
@@ -0,0 +1,13 @@
+return {
+  'catppuccin/nvim',
+  'folke/tokyonight.nvim',
+  'rebelot/kanagawa.nvim',
+  'EdenEast/nightfox.nvim',
+  { "rose-pine/neovim", name = "rose-pine" },
+  'navarasu/onedark.nvim',
+  'sainnhe/gruvbox-material',
+  'projekt0n/github-nvim-theme',
+  'shaunsingh/nord.nvim',
+  'sainnhe/sonokai',
+  'sainnhe/everforest',
+}
